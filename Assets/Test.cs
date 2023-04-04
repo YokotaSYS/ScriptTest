96dd3203098def614377cd1428bf292fb71658b1
YokotaSYS <129715620+YokotaSYS@users.noreply.github.com>
2023-04-04 13:07:01 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic
+{
+    int mp = 53;
+    public void Attck()
+    {
+        if (mp > 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+        
+
+        
+    }
+
+
+        
+    
+
+}
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //課題
+        int[] array = { 1, 3, 5, 7, 9 };
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //発展課題
+        int mp = 53;
+        Magic magic = new Magic();
+        //10回+１回ループ
+        for(int i = 0; i < 11;i++)
+        {
+            magic.Attck();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
